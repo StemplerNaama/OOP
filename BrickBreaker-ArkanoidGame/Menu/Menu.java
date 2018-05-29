762936223d517a620f47afeba7d0ebeeeba065a4
StemplerNaama <naamastempler@gmail.com>
2018-05-29 12:52:49 +0300

    upload exercises

--- BrickBreaker-ArkanoidGame/Menu/Menu.java
+++
@@ -1,9 +0,0 @@
-package Menu;
-import animation.Animation;
-
-public interface Menu<T> extends Animation {
-   void addSelection(String key, String message, T returnVal);
-   T getStatus();
-   void reset();
-   void addSubMenu(String key, String message, Menu<T> subMenu);
-}
--- BrickBreaker-ArkanoidGame/arkanoid/BlockCreator.java
+++
@@ -1,5 +0,0 @@
-package arkanoid;
-public interface BlockCreator {
-   // Create a block at the specified location.
-   Block create(int xpos, int ypos);
-}
